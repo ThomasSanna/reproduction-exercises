1f416140077b049c28629562b4cfc62dffb932bd
WadeeKT <vector.sanna@gmail.com>
2023-01-25 18:21:38 +0100

    https://manage.wix.com/account/sites?referralAdditionalInfo=Route Wix Reproduction without animation

---
+++ Wix_Repro/app.js
@@ -0,0 +1,4 @@
+const containermessites = document.querySelector(".containermessites");
+const ms = document.querySelector(".ms");
+const chs = document.querySelector(".chs");
+const body = document.querySelector(".body");
